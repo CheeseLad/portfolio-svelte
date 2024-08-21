4b3da39a94cd9dfcacd66b123b28298727aea441
Jake Farrell <jake_farrell@outlook.com>
2024-08-21 13:53:54 +0100

    mobile nav fixes

--- src/components/intro.svelte
+++ src/components/intro.svelte
@@ -9,45 +9,45 @@
 <div class="bg-gray-800 text-white py-12 text-center">
   <a href="/"><img src="jake.webp" alt="Jake Farrell" class="rounded-full w-48 h-48 mx-auto mb-4 transform hover:scale-110 transition duration-300"></a>
   <h1 class="text-3xl font-bold mb-2">Jake Farrell</h1>
-  <p class="text-lg mb-6">DCU Computer Science Student & Full-Stack Software Engineer</p>
-  <div class="flex justify-center">
+  <p class="text-lg mb-6 px-4">DCU Computer Science Student & Full-Stack Software Engineer</p>
+  <div class="flex justify-center flex-wrap">
     <a href="https://github.com/CheeseLad" target="_blank" class="hover:text-gray-700 transform hover:scale-110 transition duration-300">
-      <FontAwesomeIcon icon={faGithub} class="text-white text-3xl mr-4" />
+      <FontAwesomeIcon icon={faGithub} class="text-white text-3xl mr-4 mb-2" />
     </a>
     <a href="https://www.linkedin.com/in/jake-farrell-2a4674239" target="_blank" class="hover:text-blue-700 transform hover:scale-110 transition duration-300">
-      <FontAwesomeIcon icon={faLinkedin} class="text-white text-3xl mr-4" />
+      <FontAwesomeIcon icon={faLinkedin} class="text-white text-3xl mr-4 mb-2" />
     </a>
     <a href="https://www.instagram.com/jakefarrell2003" target="_blank" class="hover:text-pink-500 transform hover:scale-110 transition duration-300">
-      <FontAwesomeIcon icon={faInstagram} class="text-white text-3xl mr-4" />
+      <FontAwesomeIcon icon={faInstagram} class="text-white text-3xl mr-4 mb-2" />
     </a>
     <a href="mailto:jake_farrell@outlook.com" class="hover:text-blue-500 transform hover:scale-110 transition duration-300">
-      <FontAwesomeIcon icon={faEnvelope} class="text-white text-3xl" />
+      <FontAwesomeIcon icon={faEnvelope} class="text-white text-3xl mb-2" />
     </a>
   </div>
-  <div class="pt-6">
-    <a href="/" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1">
-      <FontAwesomeIcon icon={faHouse} />
+  
+  <div class="flex justify-center flex-wrap">
+    <a href="/" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1 min-w-[140px] text-center">
+      <FontAwesomeIcon icon={faHouse} class="mr-2" />
       Home
     </a>
-    <a href="/photography" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1">
-      <FontAwesomeIcon icon={faCamera} />
+    <a href="/photography" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1 min-w-[140px] text-center">
+      <FontAwesomeIcon icon={faCamera} class="mr-2" />
       Photography
     </a>
-    <!--<a href="/blog" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1">
-      <FontAwesomeIcon icon={faPenToSquare} class="mr-2" />
-      Blog
-    </a>-->
   </div>
-  <div class="pt-8">
-    <a href="{base}/Jake_Farrell_CV_2024.pdf" target="_blank" rel="noopener noreferrer" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1">
-      <FontAwesomeIcon icon={faFileLines} />
+  
+  <div class="flex justify-center flex-wrap">
+    <a href="{base}/Jake_Farrell_CV_2024.pdf" target="_blank" rel="noopener noreferrer" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1 min-w-[140px] text-center">
+      <FontAwesomeIcon icon={faFileLines} class="mr-2" />
       Résumé / CV
     </a>
-  <a href="https://home.jakefarrell.ie" target="_blank" rel="noopener noreferrer" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1">
-    <FontAwesomeIcon icon={faServer} />
-    Home Server Dashboard
-  </a>
+    <a href="https://home.jakefarrell.ie" target="_blank" rel="noopener noreferrer" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition duration-300 mt-4 mx-1 min-w-[180px] text-center">
+      <FontAwesomeIcon icon={faServer} class="mr-2" />
+      Home Server Dashboard
+    </a>
+  </div>
+  
 
-</div>
+
 </div>
 
